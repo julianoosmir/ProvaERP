9f6a9682dc6b83334e7e2d6961637f01fe89e865
juliano <julianoosmir@outlook.com>
2022-05-13 15:11:14 -0400

    fist commit

---
+++ src/main/java/com/example/provaerp/ProvaErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaErpApplication.class, args);
+    }
+
+}
